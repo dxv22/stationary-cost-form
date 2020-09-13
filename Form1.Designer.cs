403262701fb89f2bd284d863e3eb0b4580030bbc
Daxxon Vong <Daxxonvong@yahoo.co.nz>
2020-09-14 07:20:47 +1200

    Changed files

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace stationary_cost_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblPriceEach = new System.Windows.Forms.Label();
+            this.lblExtendedPrice = new System.Windows.Forms.Label();
+            this.description1 = new System.Windows.Forms.TextBox();
+            this.quantity1 = new System.Windows.Forms.TextBox();
+            this.price1 = new System.Windows.Forms.TextBox();
+            this.total1 = new System.Windows.Forms.TextBox();
+            this.lblSubTotal = new System.Windows.Forms.Label();
+            this.lblTaxRate = new System.Windows.Forms.Label();
+            this.lblSalesTax = new System.Windows.Forms.Label();
+            this.txtsubTotal = new System.Windows.Forms.TextBox();
+            this.salesTaxTxt = new System.Windows.Forms.TextBox();
+            this.taxBox = new System.Windows.Forms.TextBox();
+            this.lblGrandTotal = new System.Windows.Forms.Label();
+            this.grandTotalTxt = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.total2 = new System.Windows.Forms.TextBox();
+            this.price2 = new System.Windows.Forms.TextBox();
+            this.quantity2 = new System.Windows.Forms.TextBox();
+            this.description2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(12, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "Description";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(365, 9);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(46, 13);
+            this.lblQuantity.TabIndex = 1;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // lblPriceEach
+            // 
+            this.lblPriceEach.AutoSize = true;
+            this.lblPriceEach.Location = new System.Drawing.Point(500, 9);
+            this.lblPriceEach.Name = "lblPriceEach";
+            this.lblPriceEach.Size = new System.Drawing.Size(59, 13);
+            this.lblPriceEach.TabIndex = 2;
+            this.lblPriceEach.Text = "Price Each";
+            // 
+            // lblExtendedPrice
+            // 
+            this.lblExtendedPrice.AutoSize = true;
+            this.lblExtendedPrice.Location = new System.Drawing.Point(648, 9);
+            this.lblExtendedPrice.Name = "lblExtendedPrice";
+            this.lblExtendedPrice.Size = new System.Drawing.Size(79, 13);
+            this.lblExtendedPrice.TabIndex = 3;
+            this.lblExtendedPrice.Text = "Extended Price";
+            // 
+            // description1
+            // 
+            this.description1.Location = new System.Drawing.Point(12, 41);
+            this.description1.Name = "description1";
+            this.description1.Size = new System.Drawing.Size(252, 20);
+            this.description1.TabIndex = 4;
+            // 
+            // quantity1
+            // 
+            this.quantity1.Location = new System.Drawing.Point(318, 41);
+            this.quantity1.Name = "quantity1";
+            this.quantity1.Size = new System.Drawing.Size(141, 20);
+            this.quantity1.TabIndex = 5;
+            // 
+            // price1
+            // 
+            this.price1.Location = new System.Drawing.Point(465, 41);
+            this.price1.Name = "price1";
+            this.price1.Size = new System.Drawing.Size(141, 20);
+            this.price1.TabIndex = 8;
+            // 
+            // total1
+            // 
+            this.total1.Location = new System.Drawing.Point(612, 41);
+            this.total1.Name = "total1";
+            this.total1.ReadOnly = true;
+            this.total1.Size = new System.Drawing.Size(141, 20);
+            this.total1.TabIndex = 9;
+            // 
+            // lblSubTotal
+            // 
+            this.lblSubTotal.AutoSize = true;
+            this.lblSubTotal.Location = new System.Drawing.Point(514, 171);
+            this.lblSubTotal.Name = "lblSubTotal";
+            this.lblSubTotal.Size = new System.Drawing.Size(53, 13);
+            this.lblSubTotal.TabIndex = 22;
+            this.lblSubTotal.Text = "SubTotal:";
+            // 
+            // lblTaxRate
+            // 
+            this.lblTaxRate.AutoSize = true;
+            this.lblTaxRate.Location = new System.Drawing.Point(514, 201);
+            this.lblTaxRate.Name = "lblTaxRate";
+            this.lblTaxRate.Size = new System.Drawing.Size(54, 13);
+            this.lblTaxRate.TabIndex = 23;
+            this.lblTaxRate.Text = "Tax Rate:";
+            // 
+            // lblSalesTax
+            // 
+            this.lblSalesTax.AutoSize = true;
+            this.lblSalesTax.Location = new System.Drawing.Point(514, 233);
+            this.lblSalesTax.Name = "lblSalesTax";
+            this.lblSalesTax.Size = new System.Drawing.Size(57, 13);
+            this.lblSalesTax.TabIndex = 24;
+            this.lblSalesTax.Text = "Sales Tax:";
+            // 
+            // txtsubTotal
+            // 
+            this.txtsubTotal.Location = new System.Drawing.Point(612, 168);
+            this.txtsubTotal.Name = "txtsubTotal";
+            this.txtsubTotal.ReadOnly = true;
+            this.txtsubTotal.Size = new System.Drawing.Size(141, 20);
+            this.txtsubTotal.TabIndex = 25;
+            // 
+            // salesTaxTxt
+            // 
+            this.salesTaxTxt.Location = new System.Drawing.Point(612, 230);
+            this.salesTaxTxt.Name = "salesTaxTxt";
+            this.salesTaxTxt.ReadOnly = true;
+            this.salesTaxTxt.Size = new System.Drawing.Size(141, 20);
+            this.salesTaxTxt.TabIndex = 26;
+            // 
+            // taxBox
+            // 
+            this.taxBox.Location = new System.Drawing.Point(612, 198);
+            this.taxBox.Name = "taxBox";
+            this.taxBox.Size = new System.Drawing.Size(141, 20);
+            this.taxBox.TabIndex = 27;
+            // 
+            // lblGrandTotal
+            // 
+            this.lblGrandTotal.AutoSize = true;
+            this.lblGrandTotal.Location = new System.Drawing.Point(514, 303);
+            this.lblGrandTotal.Name = "lblGrandTotal";
+            this.lblGrandTotal.Size = new System.Drawing.Size(63, 13);
+            this.lblGrandTotal.TabIndex = 28;
+            this.lblGrandTotal.Text = "Grand Total";
+            // 
+            // grandTotalTxt
+            // 
+            this.grandTotalTxt.Location = new System.Drawing.Point(612, 300);
+            this.grandTotalTxt.Name = "grandTotalTxt";
+            this.grandTotalTxt.ReadOnly = true;
+            this.grandTotalTxt.Size = new System.Drawing.Size(141, 20);
+            this.grandTotalTxt.TabIndex = 29;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(517, 368);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 30;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOk_click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(612, 368);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 31;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // total2
+            // 
+            this.total2.Location = new System.Drawing.Point(612, 67);
+            this.total2.Name = "total2";
+            this.total2.ReadOnly = true;
+            this.total2.Size = new System.Drawing.Size(141, 20);
+            this.total2.TabIndex = 35;
+            // 
+            // price2
+            // 
+            this.price2.Location = new System.Drawing.Point(465, 67);
+            this.price2.Name = "price2";
+            this.price2.Size = new System.Drawing.Size(141, 20);
+            this.price2.TabIndex = 34;
+            // 
+            // quantity2
+            // 
+            this.quantity2.Location = new System.Drawing.Point(318, 67);
+            this.quantity2.Name = "quantity2";
+            this.quantity2.Size = new System.Drawing.Size(141, 20);
+            this.quantity2.TabIndex = 33;
+            // 
+            // description2
+            // 
+            this.description2.Location = new System.Drawing.Point(12, 67);
+            this.description2.Name = "description2";
+            this.description2.Size = new System.Drawing.Size(252, 20);
+            this.description2.TabIndex = 32;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.total2);
+            this.Controls.Add(this.price2);
+            this.Controls.Add(this.quantity2);
+            this.Controls.Add(this.description2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.grandTotalTxt);
+            this.Controls.Add(this.lblGrandTotal);
+            this.Controls.Add(this.taxBox);
+            this.Controls.Add(this.salesTaxTxt);
+            this.Controls.Add(this.txtsubTotal);
+            this.Controls.Add(this.lblSalesTax);
+            this.Controls.Add(this.lblTaxRate);
+            this.Controls.Add(this.lblSubTotal);
+            this.Controls.Add(this.total1);
+            this.Controls.Add(this.price1);
+            this.Controls.Add(this.quantity1);
+            this.Controls.Add(this.description1);
+            this.Controls.Add(this.lblExtendedPrice);
+            this.Controls.Add(this.lblPriceEach);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblDescription);
+            this.Name = "Form1";
+            this.Text = "StationaryCost";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblPriceEach;
+        private System.Windows.Forms.Label lblExtendedPrice;
+        private System.Windows.Forms.TextBox description1;
+        private System.Windows.Forms.TextBox quantity1;
+        private System.Windows.Forms.TextBox price1;
+        private System.Windows.Forms.TextBox total1;
+        private System.Windows.Forms.Label lblSubTotal;
+        private System.Windows.Forms.Label lblTaxRate;
+        private System.Windows.Forms.Label lblSalesTax;
+        private System.Windows.Forms.TextBox txtsubTotal;
+        private System.Windows.Forms.TextBox salesTaxTxt;
+        private System.Windows.Forms.TextBox taxBox;
+        private System.Windows.Forms.Label lblGrandTotal;
+        private System.Windows.Forms.TextBox grandTotalTxt;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox total2;
+        private System.Windows.Forms.TextBox price2;
+        private System.Windows.Forms.TextBox quantity2;
+        private System.Windows.Forms.TextBox description2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
+using System.Xml;
+
+namespace stationary_cost_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOk_click(object sender, EventArgs e)
+        {
+            int quantity;
+            decimal priceEach, extendedPrice, subTotal, taxRate, salesTax, grandTotal;
+
+
+            // Validate the row, if it fails then return
+            subTotal = 0;
+            salesTax = 0;
+
+            if (ValidateRow(description1, quantity1, price1, 
+                out quantity, out priceEach)) return;
+            extendedPrice = quantity * priceEach;
+            if (extendedPrice == 0m) total1.Clear();
+            else total1.Text = extendedPrice.ToString();
+            subTotal += extendedPrice;
+
+            if (ValidateRow(description2, quantity2, price2,
+                out quantity, out priceEach)) return;
+            extendedPrice = quantity * priceEach;
+            if (extendedPrice == 0m) total2.Clear();
+            else total2.Text = extendedPrice.ToString();
+            subTotal += extendedPrice;
+
+
+            // Updating subtotal textbox
+            txtsubTotal.Text = subTotal.ToString();
+
+
+            // Validate the tax rate, if it fails then return
+            if (ValidateTax(taxBox, out taxRate)) return;
+            salesTax = subTotal * taxRate;
+
+            // Displaying the taxed prices
+            salesTaxTxt.Text = salesTax.ToString();
+
+            // Displaying and calculating the grand total
+            grandTotal = subTotal + salesTax;
+            grandTotalTxt.Text = grandTotal.ToString();
+
+
+        }
+
+        private bool ValidateRow(TextBox descriptionTxtBox, TextBox quantityTxtBox, 
+            TextBox priceTxtBox, out int quantity, out decimal priceEach)
+        {
+            // Assume the quantity and price values are 0
+            quantity = 0;
+            priceEach = 0;
+
+            if ((descriptionTxtBox.Text == "") &&
+                (quantityTxtBox.Text == "") &&
+                (priceTxtBox.Text == ""))
+                return false;
+
+            // validating individual textboxes. If empty return true
+            if (ValidateIndividualText("Description", descriptionTxtBox)) return true;
+            if (ValidateIndividualText("Quantity", quantityTxtBox)) return true;
+            if (ValidateIndividualText("Price", priceTxtBox)) return true;
+
+            // All values are present
+            // Parse the quantity user input into the quantity variable
+            if (!int.TryParse(quantityTxtBox.Text, out quantity))
+            {
+                DisplayErrorMessage(
+                    "Invalid format. Quantity must be an integer", "Invalid value"
+                );
+                return true;
+            }
+
+            // Check if the quantity is between the valid ranges 
+            if (quantity < 1 || quantity > 100)
+            {
+                DisplayErrorMessage(
+                    "Invalid quantity: Quantity must be between 1 and 100", "Invalid value"
+                );
+                return true;
+            }
+
+            // Parse the priceEach user input into the priceEach variable
+            if (!decimal.TryParse(priceTxtBox.Text, NumberStyles.Currency, null, out priceEach))
+            {
+                DisplayErrorMessage(
+                    "Invalid format. Price must be a decimal", "Invalid value"
+                );
+                return true;
+            }
+
+            // Check if the priceEach is in valid range
+            if (priceEach < 0.5m || priceEach > 100000m)
+            {
+                DisplayErrorMessage(
+                    "Price each is out of range", priceTxtBox.Text
+                );
+                return true;
+            }
+
+            // All values have been checked and are all correct
+            // Return false to show there is no error
+            return false;
+        }
+
+        // Checking to see if tax rate is valid. If it is not valid return true
+        private bool ValidateTax(TextBox taxBox, out decimal taxRate)
+        {
+            // Assume tax is 0 before parsing user input
+            taxRate = 0;
+
+            // Check to see if tax is present by evaluating individual tax box
+            if (ValidateIndividualText("Tax", taxBox)) return true;
+
+            // Parse the tax value
+            if (!decimal.TryParse(taxBox.Text, out taxRate))
+            {
+                DisplayErrorMessage(
+                    "Tax rate is invalid", "Invalid value"
+                );
+                return true;
+            }
+
+            // All values have been checked and are correct
+            // Return false to show no error
+            return false;
+        }
+
+        private bool ValidateIndividualText(string name, TextBox txtBox)
+        {
+            // if textbox has a value return false
+            if (txtBox.Text != "")
+            { 
+                return false;
+            }
+            DisplayErrorMessage("Missing the " + name + " value." , "Missing value");
+            return true;
+        }
+        
+
+        private void DisplayErrorMessage(string text, string title)
+        {
+            MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
